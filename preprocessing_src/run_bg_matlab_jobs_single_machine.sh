838e6dcc780147c6688722b00ef0a52706dd365b
Yuval <yuval@yuval>
2015-12-24 03:58:36 -0500

    + bugfix submit preprocessing msdv3d, resubmit

--- preprocessing_src/run_bg_matlab_jobs_single_machine.sh
+++ preprocessing_src/run_bg_matlab_jobs_single_machine.sh
@@ -3,5 +3,6 @@ echo ==========================================
 set MatlabScript=$argv[1]
 
 source ../theano-env/bin/activate
+
 cat $MatlabScript
 matlab -nosplash -nodisplay -logfile log_$MatlabScript.txt -r "run $MatlabScript" < /dev/null > &  /dev/null 
